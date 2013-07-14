b3537ab9c6c226177fa39935ab8ba7c25dab6f23
Manraj Rangi <manraj.s.rangi@gmail.com>
2013-07-14 21:00:52 +0000

    Added web.js after hw4

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var html = fs.readFileSync('index.html').toString();
+  var html = fs.readFileSync(htmlfile).toString();
   response.send(html);
 });
 
