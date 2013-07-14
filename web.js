9981fd999bbce3045af3f09c6f16e19d291cb791
Manraj Rangi <manraj.s.rangi@gmail.com>
2013-07-14 20:56:36 +0000

    Added web.js after hw 4

--- web.js
+++ web.js
@@ -3,12 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var data = fs.readFileSync('index.html');
-  response.send(data.toString('utf-8'));
+  var html = fs.readFileSync('index.html').toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
